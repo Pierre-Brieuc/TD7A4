1e80d23b92eb4e543e9f0b43a45628cb0d771c94
root <root@LAPTOP-1NLDDJCJ>
2023-03-01 19:10:18 +0100

    First commit

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+import os
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    path = "/usr/src/app/textfile.txt"  # path inside container
+    if os.path.exists(path):
+        with open(path, "r") as f:
+            content = f.read()
+    else:
+        content = "File not found!"
+    return render_template("index.html", content=content)
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0")
